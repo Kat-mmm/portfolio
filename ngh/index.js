7b4124879b2f152eada1eddd7af1d1cf8a27a3a6
Katlego Mothibe <Kaymothibe3@gmail.com>
2022-12-01 05:03:10 +0200

    first commit

---
+++ ngh/index.js
@@ -0,0 +1,38 @@
+let productImg = document.getElementById('product-img');
+
+function show(){
+    productImg.src = './images/plain.jpg';
+}
+
+function show2(){
+    productImg.src = './images/ombre.jpg';
+}
+
+function show3(){
+    productImg.src = './images/french.jpg';
+}
+
+function show4(){
+    productImg.src = './images/plain-c.jpg';
+}
+
+function show5(){
+    productImg.src = './images/overlay.jpg';
+}
+
+function show6(){
+    productImg.src = './images/pedi.jpeg';
+}
+
+function show7(){
+    productImg.src = './images/soak.jpg';
+}
+
+function show8(){
+    productImg.src = './images/male-nails.jpeg';
+}
+
+function show9(){
+    productImg.src = './images/art.jpg';
+}
+
